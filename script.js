02328944c7d552b51cb385fac878ed3983fffd92
Pedro Giroldo <pedrogiroldo40@gmail.com>
2023-04-23 14:21:13 -0300

    solução provisória para quebra de linhas

--- script.js
+++ script.js
@@ -4,24 +4,66 @@ const client_cpf = document.getElementById("input_client_CPF");
 const client_discount = document.getElementById("input_client_discount");
 const contract_model = document.getElementById("file_input_contract");
 
+function cmToPx(valorEmCm) {
+  const larguraDoMonitorEmCm = 34; // exemplo de largura do monitor em centímetros
+  const larguraDoMonitorEmPixels = window.screen.width; // largura do monitor em pixels
+  const proporcao = larguraDoMonitorEmPixels / larguraDoMonitorEmCm; // proporção entre largura em pixels e largura em centímetros
+  const valorEmPx = valorEmCm * proporcao; // converte para pixels
+  return valorEmPx;
+}
+
+
 const generatePDF = (e) => {
   e.preventDefault();
   const { jsPDF } = window.jspdf;
-  const doc = new jsPDF("p", "pt", "letter");
+  const doc = new jsPDF("p", "pt", "a4");
 
-  // Busque o arquivo HTML e carregue o conteúdo dele
-  fetch('./teste.html')
-    .then(response => response.text())
-    .then(html => {
-      // Carregue o conteúdo HTML no documento PDF
-      doc.html(html, {
-        callback: function (doc) {
-          doc.save();
-        },
-      });
-    });
+  // Busque o arquivo de texto e carregue o conteúdo dele
+  // fetch('./teste.txt')
+  //   .then(response => response.text())
+  //   .then(txt => {
+  //     // Quebre o texto em linhas
+  //     // const lines = txt.split('\n');
+
+  //     // Adicione cada linha ao documento PDF
+  //     // lines.forEach((line, i) => {
+  //     //   doc.text(line, 20, 30 + i * 20);
+  //     // });
+
+  //     // doc.splitTextToSize(txt, 7.25, )
+
+
+  //     doc.getFontList();
+
+  //     let textLines;
+  //     textLines = doc.setFont('times', 'normal')
+  //       .setFontSize(12)
+  //       .splitTextToSize(txt.innerText, 7.25)
+
+  //     let verticalOffSet = 0.5
+  //     verticalOffSet += (textLines.length + 0.5) * 12 / 72
+  //     doc.text(textLines, 0.5, verticalOffSet + 12 / 72 )
+
+  //     doc.save('teste.pdf');
+  //   });
+
+
+  fetch('./teste.txt')
+  .then(response => response.text())
+  .then(txt => {
+    let textLines = doc.setFont('times', 'normal')
+      .setFontSize(12)
+      .splitTextToSize(txt, cmToPx(14.5));
+    // adiciona cada linha ao documento PDF
+    for (let i = 0; i < textLines.length; i++) {
+      doc.text(textLines[i], 20, 30 + i * 20);
+    }
+    doc.save('teste.pdf');
+  });
+
 };
+
 
 const client_button = document
   .getElementById("input_client_button")
-  .addEventListener("click", generatePDF); 
+  .addEventListener("click", generatePDF);
