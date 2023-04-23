b4faa961104b9c8cd29ede707529f59e19099072
LeiseR <alan.leiser@mblabs.com.br>
2023-04-23 19:24:04 -0300

    print  more than one page

--- Code/script.js
+++ Code/script.js
@@ -1,3 +1,4 @@
+const A4_HEIGHT_SIZE = 842;
 const client_name = document.getElementById("input_client_name");
 const client_rg = document.getElementById("input_client_RG");
 const client_cpf = document.getElementById("input_client_CPF");
@@ -22,33 +23,45 @@ function cmToPt(cm) {
   return pt;
 }
 
-
 const generatePDF = (e) => {
   e.preventDefault();
   const { jsPDF } = window.jspdf;
   const doc = new jsPDF("p", "pt", "a4");
-  
-fetch('../teste.txt')
-  .then(response => response.text())
-  .then(txt => {
 
-    txt = txt.replace('[name]', client_name.value);
-      txt = txt.replace('[RG]', client_rg.value);
-      txt = txt.replace('[CPF]', client_cpf.value);
-      txt = txt.replace('[%]', client_discount.value + '%');
+  fetch("../teste.txt")
+    .then((response) => response.text())
+    .then((txt) => {
+      txt = txt.replace("[name]", client_name.value);
+      txt = txt.replace("[RG]", client_rg.value);
+      txt = txt.replace("[CPF]", client_cpf.value);
+      txt = txt.replace("[%]", client_discount.value + "%");
 
-    let textLines = doc.setFont('times', 'normal')
-      .setFontSize(12)
-      .splitTextToSize(txt, cmToPt(19.5));
-    // adiciona cada linha ao documento PDF
-    for (let i = 0; i < textLines.length; i++) {
-      doc.text(textLines[i], 20, 30 + i * 20);
-    }
-    doc.save('teste.pdf');
-  });
+      let textLines = doc
+        .setFont("times", "normal")
+        .setFontSize(12)
+        .splitTextToSize(txt, cmToPt(19.5));
+      //calculo para decidir quatas páginas vão ter
+      const pages = parseInt((30 + textLines.length * 20) / A4_HEIGHT_SIZE) + 1;
+      //loop para adicionar página
+      for (let j = 0; j < pages; j++) {
+        // // adiciona cada linha ao documento PDF
+        for (let i = 0; i < textLines.length; i++) {
+          if (30 + i * 20 > A4_HEIGHT_SIZE) {
+            textLines = textLines.slice(i);
+            break;
+          }
 
+          doc.text(textLines[i], 20, 30 + i * 20);
+        }
+        if (j + 1 < pages) {
+          doc.addPage();
+        }
+      }
+      //abre o pdf em outr pagina
+      doc.output("dataurlnewwindow");
+      // // doc.save("teste.pdf");
+    });
 };
-
 
 const client_button = document
   .getElementById("input_client_button")
