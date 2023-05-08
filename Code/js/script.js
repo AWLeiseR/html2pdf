a873f50a7b909373e99ee9f043298c81440326b4
LeiseR <alan.leiser@mblabs.com.br>
2023-05-08 17:59:12 -0300

    add new inputs

--- Code/js/script.js
+++ Code/js/script.js
@@ -7,7 +7,8 @@ const bottomMargin = 40; // margem inferior em pontos
 const lineHeight = 20; // altura de linha em pontos
 
 const client_name = document.getElementById("input_client_name");
-const client_rg = document.getElementById("input_client_RG");
+const client_uc = document.getElementById("input_client_UC");
+const client_email = document.getElementById("input_client_email");
 const client_cpf = document.getElementById("input_client_CPF");
 const client_address = document.getElementById("input_client_address");
 const client_discount = document.getElementById("input_client_discount");
@@ -60,6 +61,7 @@ function formatDate(offsetYear) {
 }
 
 function addTextToDoc(doc, text) {
+  let isBold = false;
   const font = doc.setFont("times", "normal");
   doc.setFontSize(11);
   const lines = doc.splitTextToSize(text, cmToPt(16), { font });
@@ -75,6 +77,7 @@ function addTextToDoc(doc, text) {
     }
     const boldLine = lines[i].split(/(<b>.*?<\/b>)/gim);
     if (boldLine.length > 1) {
+      isBold = false;
       let offset = 0;
       for (let j = 0; j < boldLine.length; j++) {
         if (boldLine[j].startsWith("<b>") && boldLine[j].endsWith("</b>")) {
@@ -88,6 +91,7 @@ function addTextToDoc(doc, text) {
         }
       }
     } else if (boldLine[0].indexOf("<b>") > -1) {
+      isBold = true;
       let offset = 0;
       let semiLineBold = boldLine[0].split(/(<b>.*)/g);
       for (let j = 0; j < semiLineBold.length; j++) {
@@ -102,6 +106,7 @@ function addTextToDoc(doc, text) {
         }
       }
     } else if (boldLine[0].indexOf("</b>") > -1) {
+      isBold = false;
       let offset = 0;
       let semiLineBold = boldLine[0].split(/(.*<\/b>)/g);
       for (let j = 0; j < semiLineBold.length; j++) {
@@ -115,6 +120,9 @@ function addTextToDoc(doc, text) {
           offset = 0;
         }
       }
+    } else if (isBold) {
+      doc.setFont("times", "bold");
+      doc.text(boldLine, cmToPt(3), y);
     } else {
       doc.setFont("times", "normal");
       doc.text(boldLine, cmToPt(3), y);
@@ -132,24 +140,24 @@ const generatePDF = (e) => {
     format: [A4_WIDTH_SIZE, A4_HEIGHT_SIZE],
   });
 
-  fetch("../modelo_contrato.txt")
+  fetch("../novo_contrato.txt")
     .then((response) => response.text())
     .then((txt) => {
-      const regex_name = /NOME_LOCADOR/g;
-      const regex_address = /ENDERECO_LOCADOR/g;
-      const regex_rg = /LOCADOR_RG/g;
+      const regex_name = /LOCADOR_NOME/g;
+      const regex_address = /LOCADOR_ENDERECO/g;
+      const regex_UC = /LOCADOR_UC/g;
       const regex_cpf = /LOCADOR_CPF/g;
       const regex_discount = /DESCONTO_LOCADOR/g;
       const regex_date_start = /DATA_HOJE/g;
-      const regex_date_end = /DATA_FIM/g;
+      const regex_email = /LOCADOR_EMAIL/g;
 
       txt = txt.replace(regex_name, client_name.value);
-      txt = txt.replace(regex_rg, client_rg.value);
+      txt = txt.replace(regex_UC, client_uc.value);
       txt = txt.replace(regex_address, client_address.value);
       txt = txt.replace(regex_cpf, client_cpf.value);
       txt = txt.replace(regex_discount, client_discount.value + "%");
       txt = txt.replace(regex_date_start, formatDate(0));
-      txt = txt.replace(regex_date_end, formatDate(5));
+      txt = txt.replace(regex_email, client_email.value);
       addTextToDoc(doc, txt);
       doc.setLineWidth(500);
       // doc.save("contrato.pdf");
